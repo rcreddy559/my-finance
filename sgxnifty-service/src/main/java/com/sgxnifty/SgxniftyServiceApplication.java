697883c0e438c65f7043e9cd102e957c32359e56
Ravichandra Reddy Penumuru <ravichandra.p@outlook.com>
2021-03-23 17:34:06 +0100

    new nifty pro

---
+++ sgxnifty-service/src/main/java/com/sgxnifty/SgxniftyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sgxnifty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgxniftyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgxniftyServiceApplication.class, args);
+	}
+
+}
